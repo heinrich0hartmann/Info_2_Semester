a05c48405cc9abb0c341002dc2f03bca1a314d54
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-05-02 16:43:58 +0200

    Head Datei für Auto schreiben
    Standard Kostruktor
    Allgemeiner Konstruktor
    Kopier Konstruktor
    Destruktor
    Getter
    private

--- Übungen/C.4.12.1_Klasse_Auto/Auto_Headder.h
+++ Übungen/C.4.12.1_Klasse_Auto/Auto_Headder.h
@@ -1,1 +1,48 @@
 #pragma once
+
+#include <string>
+
+using namespace std;
+
+class Auto
+{
+public:
+	// Standard-Konstruktor
+	Auto();
+
+	// Allgemeiner-Konstruktor
+	Auto(int radzahl, const string& farbe, int ps, int anzahl_sitze, double spritverbrauch, double tankinhalt, double tankgroesse);
+
+	// Kopier-Konsruktor
+	Auto(const Auto& a);
+
+	//Destruktor
+	~Auto();
+
+	//Methode fahren()
+	float fahren(float strecke);
+
+	// Getter-Methoden
+	int radzahl() const;
+	string farbe() const;
+	int ps() const;
+	int anzahl_sitze() const;
+
+	// In Liter pro 100km 
+	double spritverbrauch() const;
+
+	double tankinhalt() const;
+	double tankgroesse() const;
+
+	// Setter-Methode
+	bool tanken(double tankinhalt);
+
+private:
+	int m_radzahl;
+	string m_farbe;
+	int m_ps;
+	int m_anzahl_sitze;
+	double m_spritverbrauch;
+	double m_tankinhalt;
+	double m_tankgroesse;
+};

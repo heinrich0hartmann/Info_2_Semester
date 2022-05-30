2df05b7037d136c8e3ff544a27211f3e5e9c0c4a
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-05-30 13:05:36 +0200

    Complex_Test komplett

--- Übungen/C.4.14.1_Complex_4.15.1/ComplexTest.cpp
+++ Übungen/C.4.14.1_Complex_4.15.1/ComplexTest.cpp
@@ -10,10 +10,32 @@ int main()
 {
 	try
 	{
-	
-	}
-	catch (const std::exception&)
-	{
+		Complex z1(0, 0);
+		Complex z2(1, 1);
+		Complex z3(2, 2);
+		cout << "z2 = 1+1i" << endl;
+		cout << "z3 = 2+2i" << endl;
+		//Eingabetest
+		cout << "Geben Sie eine komplexe Zahl ein: ";
+		cin >> z1;
+		cout << "z1: " << z1;
+		//Subtraktionstest
+		Complex Z = z1 - z2;
+		cout << "z1 - z2: " << Z; 
+		//Divisionstest
+		Z = z1 / z3;
+		cout << "z1 / z3:" << Z; << endl;
+		//invalid test
+		z3.setRe(0);
+		z3.setIm(0);
+		Z = z1 / z3;
 
 	}
+	catch (invalid_argument &ex)
+	{
+		cout << ex.what() << endl;
+	}
+	system("pause");
+	return 0;
+
 }

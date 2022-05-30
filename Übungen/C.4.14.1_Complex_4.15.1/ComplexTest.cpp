b17fc946c75ca7c6899d6e7d11dc2c0a9b6b4140
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-05-30 12:50:00 +0200

    initialisierung ComplexTest

---
+++ Übungen/C.4.14.1_Complex_4.15.1/ComplexTest.cpp
@@ -0,0 +1,19 @@
+#include "Complex.h"
+
+#include <iostream>
+
+using namespace std;
+
+//Das Testprogramm für die Klasse Complex und den überladenen Operator
+
+int main()
+{
+	try
+	{
+	
+	}
+	catch (const std::exception&)
+	{
+
+	}
+}

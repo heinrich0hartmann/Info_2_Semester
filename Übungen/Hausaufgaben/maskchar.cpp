3c3ad118480184eb9476e2407b76b6c9a44cfc26
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-05-30 13:50:31 +0200

    Masken Programm

---
+++ Übungen/Hausaufgaben/maskchar.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+const int x = 32;
+
+// Konvertiert einen Kleinbuchtaben zu einem Groﬂbuchstaben.
+char* zuGroﬂbuchstabe(char* a)
+{
+	a[0] = a[0] & ~x; //Bin‰roperatoren
+	return a;
+}
+
+// Konvertiert einen Groﬂbuchstaben zu einem Kleinbushtaben.
+char* zuKleinbuchstabe(char* a)
+{
+	a[0] = a[0] | x; //Bin‰roperatoren
+	return a;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << endl << "Geben Sie einen Klein- oder Gro\341buchstaben ein." << endl << endl;
+		return 1;
+	}
+	char* buchstabe(argv[1]);
+	if (buchstabe[1] != '\0')
+	{
+		cout << endl << "Geben Sie einen Klein- oder Groﬂbuchstaben ein." << endl << endl;
+		return 1;
+	}
+
+	if (buchstabe[0] > 97) // Ab Stelle 97 in der ASCII Tabelle fangen klein Buchstaben an!
+	{
+		cout << zuGroﬂbuchstabe(buchstabe) << endl;
+		return 0;
+	}
+	else
+	{
+		cout << zuKleinbuchstabe(buchstabe) << endl;
+		return 0;
+	}
+	// Zeichen war nicht aus dem Alphabet.
+	cout << endl << "Kein Zeichen aus dem Alphabet oder Umlaut wurde eingegeben!" << endl << endl;
+	system("pause");
+	return 1;
+}
+
